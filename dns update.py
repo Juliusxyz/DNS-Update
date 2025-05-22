da01fc26a9595c37be688b279099507d7cce15ed
Julius <julius@vize.host>
2025-05-22 11:56:33 +0200

    Changed some comments

--- dns update.py
+++ dns update.py
@@ -1,52 +1,52 @@
 import requests
 import time
 
-# Konfiguration
-API_TOKEN = ''  # Ersetze mit deinem Cloudflare API Token
-ZONE_ID = ''      # Ersetze mit deiner Zone ID
-RECORD_ID = ''    # Ersetze mit der Record ID des DNS-Eintrags
-DNS_NAME = ''      # Ersetze mit dem DNS-Namen, den du aktualisieren möchtest
+# Confuguration
+API_TOKEN = ''    # Cloudflare API Token
+ZONE_ID = ''      # Zone ID
+RECORD_ID = ''    # Record ID from DNS entry
+DNS_NAME = ''     # DNS name, to update
 
 def get_public_ip():
-    #Holt die aktuelle öffentliche IP-Adresse.
+    # get current public ip
     response = requests.get('https://api.ipify.org?format=json')
     return response.json()['ip']
 
 def update_dns_record(ip):
-    #Aktualisiert den DNS-Record mit der neuen IP-Adresse.
+    # update DNS record with the new IP adresse.
     url = f'https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records/{RECORD_ID}'
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {API_TOKEN}'
     }
     data = {
-        'type': 'A',  # Typ des DNS-Records
+        'type': 'A',  # DNS record type
         'name': DNS_NAME,
         'content': ip,
         'ttl': 1,  # TTL (Time to Live)
-        'proxied': False  # Setze auf True, wenn du Cloudflare Proxy verwenden möchtest
+        'proxied': False  # change to true, if you want to use cloudflare proxy
     }
     
     response = requests.put(url, headers=headers, json=data)
     
     if response.status_code == 200:
-        print(f'DNS-Record erfolgreich aktualisiert auf: {ip}')
+        print(f'DNS record successfully updated to: {ip}')
     else:
-        print('Fehler beim Aktualisieren des DNS-Records:', response.json())
+        print('Error while updating DNS entry:', response.json())
 
 def main():
     current_ip = get_public_ip()
-    print(f'Aktuelle IP: {current_ip}')
+    print(f'Current ip: {current_ip}')
     
     while True:
-        time.sleep(300)  # 5 Minuten warten
+        time.sleep(300)  # wait 5 minutes
         new_ip = get_public_ip()
         if new_ip != current_ip:
-            print(f'IP hat sich geändert: {new_ip}')
+            print(f'IP adress has changed: {new_ip}')
             update_dns_record(new_ip)
             current_ip = new_ip
         else:
-            print('IP hat sich nicht geändert.')
+            print('IP adress didn't change.')
 
 if __name__ == '__main__':
     main()
